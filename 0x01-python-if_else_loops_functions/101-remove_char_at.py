1c28268e61d9f8c8ab215ec9917ef77409580863
Assel Kebede <asselmeseret@gmail.com>
2022-06-28 12:41:28 -0700

    Write a function that creates a copy of the string, removing the character at the position n (not the Python way, the “C array index”)

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    part = str[:n]
-    parts = str[n+1:]
-    return part + parts
+    fixed_str = ""
+    for i, letter in enumerate(str):
+        if i != n:
+            fixed_str += letter
+    return fixed_str
