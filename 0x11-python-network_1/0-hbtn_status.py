94f8185da58baf0b24def31652421ac1026f1030
Assel kebede <asselmeseret@gmail.com>
2022-10-21 04:23:39 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" module that fetches https://intranet.hbtn.io/status"""
-import urllib.request as request
-
-
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as result:
-        body = result.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
+    import urllib.request as request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
